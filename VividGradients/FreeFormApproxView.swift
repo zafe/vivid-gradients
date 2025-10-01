4064920d2697f97592dd0e0883eecfe62645f9d7
Fernando Zafe <fernandozafe@gmail.com>
2025-10-01 01:46:35 -0300

    Added Noise Layer

--- VividGradients/FreeFormApproxView.swift
+++ VividGradients/FreeFormApproxView.swift
@@ -7,6 +7,51 @@
 
 import SwiftUI
 
+struct NoiseLayer: View {
+    let opacity: Double
+    let granularity: Double
+
+    init(opacity: Double = 0.08, granularity: Double = 0.5) {
+        self.opacity = opacity
+        self.granularity = granularity
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            Image(decorative: noiseImage(size: geometry.size, granularity: granularity), scale: 1.0)
+                .resizable()
+                .opacity(opacity)
+                .blendMode(.overlay)
+                .ignoresSafeArea()
+        }
+    }
+
+    private func noiseImage(size: CGSize, granularity: Double) -> CGImage {
+        let width = Int(size.width * granularity)
+        let height = Int(size.height * granularity)
+        let dataSize = width * height
+        var pixelData: [UInt8] = [UInt8](repeating: 0, count: dataSize)
+
+        for i in 0..<dataSize {
+            let randomValue = UInt8.random(in: 0...255)
+            pixelData[i] = randomValue
+        }
+
+        let context = CGContext(
+            data: &pixelData,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: width,
+            space: CGColorSpaceCreateDeviceGray(),
+            bitmapInfo: CGImageAlphaInfo.none.rawValue
+        )
+
+        let cgImage = context!.makeImage()!
+        return cgImage
+    }
+}
+
 struct FreeformApproxView: View {
     @State private var nodePositions: [CGPoint] = [
         CGPoint(x: 0.2, y: 0.3),
@@ -14,69 +59,73 @@ struct FreeformApproxView: View {
         CGPoint(x: 0.4, y: 0.7),
         CGPoint(x: 0.85, y: 0.6)
     ]
-    
+
     let nodes: [(color: Color, radius: CGFloat)] = [
-        (.red.opacity(0.9), 0.5),
+        (.orange.opacity(0.9), 0.5),
         (.red.opacity(0.9), 0.45),
-        (.red.opacity(0.9), 0.6),
+        (.orange.opacity(0.9), 0.6),
         (.red.opacity(0.85), 0.4)
     ]
-    
+
     let staticNodes: [(position: CGPoint, radius: CGFloat)] = [
         (CGPoint(x: 0.3, y: 0.4), 0.3),
         (CGPoint(x: 0.7, y: 0.3), 0.25),
         (CGPoint(x: 0.5, y: 0.6), 0.35),
         (CGPoint(x: 0.8, y: 0.7), 0.28)
     ]
-    
+
     let blendMode: BlendMode = .overlay
-    
+
     var body: some View {
-        GeometryReader { geo in
-            ZStack {
-                // Static white nodes
-                ForEach(0..<staticNodes.count, id: \.self) { i in
-                    let n = staticNodes[i]
-                    RadialGradient(
-                        gradient: Gradient(colors: [.black.opacity(0.7), .black.opacity(0.0)]),
-                        center: .center,
-                        startRadius: 10,
-                        endRadius: min(geo.size.width, geo.size.height) * n.radius
-                    )
-                    .frame(width: geo.size.width, height: geo.size.height)
-                    .position(
-                        x: n.position.x * geo.size.width,
-                        y: n.position.y * geo.size.height
-                    )
-                    .blendMode(blendMode)
+        ZStack {
+            GeometryReader { geo in
+                ZStack {
+                    // Static white nodes
+                    ForEach(0..<staticNodes.count, id: \.self) { i in
+                        let n = staticNodes[i]
+                        RadialGradient(
+                            gradient: Gradient(colors: [.black.opacity(0.7), .black.opacity(0.0)]),
+                            center: .center,
+                            startRadius: 10,
+                            endRadius: min(geo.size.width, geo.size.height) * n.radius
+                        )
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .position(
+                            x: n.position.x * geo.size.width,
+                            y: n.position.y * geo.size.height
+                        )
+                        .blendMode(blendMode)
+                    }
+
+                    // Animated colored nodes
+                    ForEach(0..<nodes.count, id: \.self) { i in
+                        let n = nodes[i]
+                        RadialGradient(
+                            gradient: Gradient(colors: [n.color, n.color.opacity(0.0)]),
+                            center: .center,
+                            startRadius: 20,
+                            endRadius: min(geo.size.width, geo.size.height) * n.radius
+                        )
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .position(
+                            x: nodePositions[i].x * geo.size.width,
+                            y: nodePositions[i].y * geo.size.height
+                        )
+                        .blendMode(blendMode)
+                    }
                 }
-                
-                // Animated colored nodes
-                ForEach(0..<nodes.count, id: \.self) { i in
-                    let n = nodes[i]
-                    RadialGradient(
-                        gradient: Gradient(colors: [n.color, n.color.opacity(0.0)]),
-                        center: .center,
-                        startRadius: 20,
-                        endRadius: min(geo.size.width, geo.size.height) * n.radius
-                    )
-                    .frame(width: geo.size.width, height: geo.size.height)
-                    .position(
-                        x: nodePositions[i].x * geo.size.width,
-                        y: nodePositions[i].y * geo.size.height
-                    )
-                    .blendMode(blendMode)
-                }
+                .compositingGroup()
+                .blur(radius: 30)
             }
-            .compositingGroup()
-            .blur(radius: 30)
-            .ignoresSafeArea()
-            .onAppear {
-                animateRandomly()
-            }
+
+            NoiseLayer(opacity: 0.1, granularity: 0.7)
+        }
+        .ignoresSafeArea()
+        .onAppear {
+            animateRandomly()
         }
     }
-    
+
     private func animateRandomly() {
         withAnimation(.easeInOut(duration: 3.0).repeatForever(autoreverses: true)) {
             for i in 0..<nodePositions.count {
